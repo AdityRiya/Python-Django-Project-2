4e70344a1ec38e0b444ef8e2160ae322b9cded81
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-21 23:55:09 +0600

    Update models.py

--- pyDjangoProject2/DjangoApp1/models.py
+++ pyDjangoProject2/DjangoApp1/models.py
@@ -1,5 +1,8 @@
+import email
 from django.db import models
 
 # Create your models here.
-# class User(models.model):
-    
+class User(models.Model):
+    first_name=models.CharField(max_length=128)
+    last_name=models.CharField(max_length=128)
+    email
