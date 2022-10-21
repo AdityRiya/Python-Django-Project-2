42d3f8481262a2b606617046c55d546cf537af66
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2022-10-21 23:56:21 +0600

    Update models.py

--- pyDjangoProject2/DjangoApp1/models.py
+++ pyDjangoProject2/DjangoApp1/models.py
@@ -1,8 +1,9 @@
 import email
+from enum import unique
 from django.db import models
 
 # Create your models here.
 class User(models.Model):
     first_name=models.CharField(max_length=128)
     last_name=models.CharField(max_length=128)
-    email
+    email =models.EmailField(max_length=264,unique = True)
